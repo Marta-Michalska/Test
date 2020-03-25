8044086cb64bb212103f7236ca00f0fbd9f07ac2
Tusiak <konecka.marta@gmail.com>
2020-03-25 11:07:47 +0100

    2

---
+++ src/com/company/Main.java
@@ -0,0 +1,87 @@
+package com.company;
+
+import java.math.BigInteger;
+import java.sql.SQLOutput;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+       /* int[] tab = {20, 13, 10, 1, 100, 20, 102, 4, 10, 17};
+
+
+        for (int a = 0; a < tab.length - 1; a++) {
+
+            for (int i = 0; i < tab.length - 1; i++)
+
+                if (tab[i] > tab[i + 1]) {
+                    int temp = tab[i + 1];
+                    tab[i + 1] = tab[i];
+                    tab[i] = temp;
+
+                }
+        }
+
+        //Arrays.sort(tab);
+        System.out.println(Arrays.toString(tab));
+
+
+
+
+        int tabL = tab.length;
+        int max = tab[0];
+        int min = tab[0];
+        double sum = tab[0];
+        for (int i = 0; i < tabL; i++) {
+
+            if (max < tab[i]) {
+                max = tab[i];
+            }
+
+
+            if (min > tab[i]) {
+                min = tab[i];
+            }
+            sum = sum + tab[i];
+        }
+
+
+        System.out.println(max);
+        System.out.println(min);
+        System.out.println(sum / tab.length); */
+
+
+
+
+
+
+
+      /*  for (int i = 1; i <= 100; i++) {
+
+            if (i % 3 == 0 && i % 5 != 0) {
+
+                System.out.println("Fizz");
+            }
+
+            else if (i % 3 != 0 && i%5==0){
+
+                System.out.println("Buzz");
+            }
+
+            else if (i %3 ==0 && i% 5 ==0){
+
+                System.out.println("FizzBuzz");
+            }
+            else{
+                System.out.println(i);
+            }
+
+
+        }*/
+
+
+    }
+}
