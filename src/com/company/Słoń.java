b2599a24551aad0ee78414c166b199d36c508cd1
Wojciech Biłka <wojciech.bilka@gmail.com>
2020-03-25 12:44:23 +0100

    Addeed Glos implementation

--- src/com/company/Słoń.java
+++ src/com/company/Słoń.java
@@ -1,9 +1,14 @@
 package com.company;
 
-public class Słoń implements Łapy {
+public class Słoń implements Glos, Łapy {
 
     @Override
     public int ilośćŁap() {
         return 4;
+    }
+
+    @Override
+    public void dajGlos() {
+        System.out.println("Whhhhuuuuuufffff");
     }
 }
