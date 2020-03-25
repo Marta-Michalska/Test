fba70b2b4ef8244b77486b9ab54585a31655a683
Wojciech Biłka <wojciech.bilka@gmail.com>
2020-03-25 12:43:50 +0100

    Addeed Glos implementation

--- src/com/company/Lew.java
+++ src/com/company/Lew.java
@@ -1,8 +1,13 @@
 package com.company;
 
-public class Lew implements Łapy{
+public class Lew implements Glos, Łapy{
     @Override
     public int ilośćŁap() {
         return 4;
+    }
+
+    @Override
+    public void dajGlos() {
+        System.out.println("Groar Groar");
     }
 }
